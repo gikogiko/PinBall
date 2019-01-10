9a1e22eb38e54239ba06a3bd5a6a6e8b0b77842e
gikogiko <blend8e@gmail.com>
2019-01-10 22:58:07 +0900

    Lesson5課題の提出

---
+++ Assets/scoreControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreControl : MonoBehaviour {
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //スコアを保持
+    private int totalScore;
+
+    // Use this for initialization
+    void Start () {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+        //スコアの初期値設定と得点表示
+        totalScore = 0;
+        SetScore();
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string collisionGameObjectTag = collision.gameObject.tag;
+
+        // スコアを加算
+        if (collisionGameObjectTag == "SmallStarTag"){
+            totalScore += 10;
+        }else if (collisionGameObjectTag == "LargeStarTag"){
+            totalScore += 20;
+        }else if (collisionGameObjectTag == "SmallCloudTag"){
+            totalScore += 30;
+        }else if (collisionGameObjectTag == "LargeCloudTag"){
+            totalScore += 40;
+        }
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        this.scoreText.GetComponent<Text>().text = string.Format("Score:{0}", totalScore);
+    }
+}
